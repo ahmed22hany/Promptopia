a30c1b8b4ab749b7b9134f0dc4bee3e1f09292f6
Ahmed Hany <ahaany5673@gmail.com>
2023-06-06 03:29:05 +0200

    Bug in Feed

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,10 +4,23 @@ import Prompt from "@models/prompt";
 export const GET = async (request) => {
     try {
         await connectToDB();
+        const prompts = await Prompt.find().populate({
+            path: "creator"
+        });
 
-        const prompts = await Prompt.find({}).populate('creator');
+        const response = new Response(JSON.stringify(prompts), {
+            status: 200,
+        });
 
-        return new Response(JSON.stringify(prompts), { status: 200 });
+        // Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(req.url);
+        url.searchParams.set("t", Date.now());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+
+        return response;
     } catch (error) {
         return new Response(JSON.stringify('Failed to fetch all prompts'), { status: 500 });
     }
